f45bae9f9bbcdcf665c1046c53ee716fba47f29a
narutoshippuden123 <kazuto123456789101112@gmail.com>
2025-04-03 15:51:42 +0800

    initial commit

--- 2nd_portfolio/vite.config.js
+++ 2nd_portfolio/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 3000
-  }
+  base: process.env.VITE_BASE_PATH || "/2ndtry-portfolio"
 })
